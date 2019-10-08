32050b874264c5b28e36408c49d1337d68bf7d13
Abdelrahman Mohamed <elhedody@AbdelraiMac3030.lan>
2019-10-08 13:48:57 +0200

    Updates

---
+++ precache-manifest.ef2d7118dc15d38d93d4cfc8249c2c77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0245fa3615b5d64523c81bd04237ccfa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "363859e44d5d042d4501",
+    "url": "/monsters-rolodex/static/css/main.16c43d4a.chunk.css"
+  },
+  {
+    "revision": "ae19bbf4f0132e1d42f3",
+    "url": "/monsters-rolodex/static/js/2.c00470ae.chunk.js"
+  },
+  {
+    "revision": "363859e44d5d042d4501",
+    "url": "/monsters-rolodex/static/js/main.ba821168.chunk.js"
+  },
+  {
+    "revision": "c4a009f5d3a5fc9c4346",
+    "url": "/monsters-rolodex/static/js/runtime-main.205b164d.js"
+  }
+]);
